bbd3bd18582b7914fcb551869e8a17daf24be71b
Aurel Medvegy <aurel.medvegy@ctoolkit.org>
2017-07-22 08:09:40 +0200

    LocalResourceProvider interface has been split to support GET and LIST operation separately. #TECO-22

--- src/main/java/org/ctoolkit/restapi/client/provider/LocalListResourceProvider.java
+++ src/main/java/org/ctoolkit/restapi/client/provider/LocalListResourceProvider.java
@@ -18,10 +18,10 @@
 
 package org.ctoolkit.restapi.client.provider;
 
-import org.ctoolkit.restapi.client.Identifier;
-
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
@@ -30,45 +30,43 @@ import java.util.Map;
  * The implementation of concrete type is optional and will be used once binded in configuration.
  * <p/>
  * To bind a concrete implementation use this in your guice module: <pre>{@code
- *  bind( new TypeLiteral<LocalResourceProvider<MyResource>>(){} )
- *      .to( MyResourceProvider.class );
+ *  bind( new TypeLiteral<LocalListResourceProvider<MyResource>>(){} )
+ *      .to( MyListResourceProvider.class );
  * }</pre>
  *
- * @param <T> the type of the resource
+ * @param <T> the type of the resource within list
  * @author <a href="mailto:aurel.medvegy@ctoolkit.org">Aurel Medvegy</a>
  */
-public interface LocalResourceProvider<T>
+public interface LocalListResourceProvider<T>
 {
     /**
-     * Retrieve a local resource to avoid a remote call.
-     * Note: if this method returns <tt>null</tt> the {@link #persist(Object, Identifier, Map, Locale, Long)}
+     * Retrieve a local list of resource to avoid a remote call.
+     * Note: if this method returns <tt>null</tt> the {@link #persistList(List, Map, Locale, Long)}
      * will be called once a remote call returns a non <tt>null</tt> value. Processed in the same thread.
      *
-     * @param identifier the unique (root) identifier of the resource
-     * @param parameters the optional resource parameters
-     * @param locale     the language the client has configured to prefer in results if applicable
+     * @param parameters       the optional resource parameters
+     * @param locale           the language the client has configured to prefer in results if applicable
+     * @param lastModifiedDate the last modified date of the remote resource if any
      * @return the locally cached or stored resource if exist, otherwise returns <tt>null</tt>
      */
-    T get( @Nonnull Identifier identifier,
-           @Nullable Map<String, Object> parameters,
-           @Nullable Locale locale );
+    List<T> list( @Nullable Map<String, Object> parameters,
+                  @Nullable Locale locale,
+                  @Nullable Date lastModifiedDate );
 
     /**
      * Optionally, persist the resource instance as a result of the remote call.
-     * This method will be called in case if {@link #get(Identifier, Map, Locale)}
+     * This method will be called in case if {@link #list(Map, Locale, Date)}
      * returns <tt>null</tt> and for a non null result of the remote call. Processed in the same thread.
      * <p/>
      * Note: prefer asynchronous implementation.
      *
-     * @param instance   the resource instance to be either persisted or cached
-     * @param identifier the unique (root) identifier of the resource
+     * @param list       the list of resources to be either persisted or cached
      * @param parameters the optional resource parameters
      * @param locale     the language the client has configured to prefer in results if applicable
      * @param lastFor    the time in milliseconds how long to keep the resource cached, <tt>null</tt> for undefined
      */
-    void persist( @Nonnull T instance,
-                  @Nonnull Identifier identifier,
-                  @Nullable Map<String, Object> parameters,
-                  @Nullable Locale locale,
-                  @Nullable Long lastFor );
+    void persistList( @Nonnull List<T> list,
+                      @Nullable Map<String, Object> parameters,
+                      @Nullable Locale locale,
+                      @Nullable Long lastFor );
 }
